c707dfb22e3ae6e7fd63d20a0115e0d8b28569a9
heroZkb <592329619@qq.com>
2016-09-08 17:32:12 +0800

    add author name
    add zkb

--- xapian-core/matcher/valuegtpostlist.cc
+++ xapian-core/matcher/valuegtpostlist.cc
@@ -19,7 +19,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
  */
-
+//add zkb
 #include <config.h>
 
 #include "valuegtpostlist.h"
--- xapian-core/matcher/valuegtpostlist.h
+++ xapian-core/matcher/valuegtpostlist.h
@@ -18,7 +18,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
  */
-
+//add zkb
 #ifndef XAPIAN_INCLUDED_ValueGtPostList_H
 #define XAPIAN_INCLUDED_ValueGtPostList_H
 
